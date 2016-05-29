6c795c3bb0bf9c637614b18df0c06f2d4a0d5a15
anton <antonpaquin@gmail.com>
2016-05-29 11:17:14 +0000

    Sidebar is good and styled!

--- app/controllers/adventure_controller.rb
+++ app/controllers/adventure_controller.rb
@@ -2,7 +2,6 @@ class AdventureController < ApplicationController
   def get
     @adventure = Adventure.find(id=params[:id])
     @advId = params[:id]
-    render layout: 'headerbar'
   end
 
   def all
